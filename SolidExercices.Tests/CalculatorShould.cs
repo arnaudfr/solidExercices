1773a0768ab614bbb697dfd8c26d1955e84e24f4
FROSSARD ARNAUD p1613045 <arnaud.frossard@gmail.com>
2017-06-22 17:39:36 +0200

    Ajout de tests avec plus de 2 valeur par opération

--- SolidExercices.Tests/CalculatorShould.cs
+++ SolidExercices.Tests/CalculatorShould.cs
@@ -36,5 +36,37 @@ namespace SolidExercices.Tests
             var result = calculator.Calculate("9/3");
             Check.That(result).IsEqualTo(3m);
         }
+
+        [Test]
+        public void CalculateASumMulti()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Calculate("1+2,3+12");
+            Check.That(result).IsEqualTo(15.3m);
+        }
+
+        [Test]
+        public void CalculateASubMulti()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Calculate("1-2-1000");
+            Check.That(result).IsEqualTo(-1001m);
+        }
+
+        [Test]
+        public void CalculateAProdMulti()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Calculate("1*2,3*12");
+            Check.That(result).IsEqualTo(27.6m);
+        }
+
+        [Test]
+        public void CalculateADivMulti()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Calculate("1000/10/10");
+            Check.That(result).IsEqualTo(10m);
+        }
     }
 }
